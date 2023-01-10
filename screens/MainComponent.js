4425a05ee9cffdedbe451b68b8d43641616355e7
App317 <101300095+App317@users.noreply.github.com>
2023-01-10 18:37:13 -0500

    RenderCampsite and InfoScreen

--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -1,11 +1,27 @@
 import { useState } from 'react';
+import { View } from 'react-native';
 import { CAMPSITES } from '../shared/campsites';
+import CampsiteInfoScreen from './CampsiteInfoScreen';
 import DirectoryScreen from './DirectoryScreen';
 
 const Main = () => {
   const [campsites, setCampsites] = useState(CAMPSITES);
+  const [selectedCampsiteId, setSelectedCampsiteId] = useState();
 
-  return <DirectoryScreen campsites={campsites} />;
+  return (
+    <View style={{ flex: 1 }}>
+      <DirectoryScreen
+        campsites={campsites}
+        /*onPress finds what campsite been selected from directory (arrow function is an onPress event) */
+        onPress={(campsiteId) => setSelectedCampsiteId(campsiteId)}
+      />
+      <CampsiteInfoScreen
+        campsite={
+          campsites.filter((campsite) => campsite.id === selectedCampsiteId)[0]
+        }
+      />
+    </View>
+  );
 };
 
 export default Main;
