e20f1c1ed14070ba34a8a6d442a3481b43345e2f
App317 <101300095+App317@users.noreply.github.com>
2023-01-17 20:24:42 -0500

    Redux Adding Favorites

--- screens/AboutScreen.js
+++ screens/AboutScreen.js
@@ -1,9 +1,8 @@
-import { useState } from 'react';
 import { ScrollView, Text, View } from 'react-native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { createDrawerNavigator } from '@react-navigation/drawer';
 import { Card, Avatar, ListItem } from 'react-native-elements';
-import { PARTNERS } from '../shared/partners';
+import { useSelector } from 'react-redux';
+import { baseUrl } from '../shared/baseUrl';
+import Loading from '../components/LoadingComponent';
 
 const Mission = () => {
   return (
@@ -24,7 +23,33 @@ const Mission = () => {
 };
 
 const AboutScreen = () => {
-  const [partners, setPartners] = useState(PARTNERS);
+  const partners = useSelector((state) => state.partners);
+
+  if (partners.isLoading) {
+    return (
+      <ScrollView>
+        <Mission />
+        <Card>
+          <Card.Title>Community Partners</Card.Title>
+          <Card.Divider />
+          <Loading />
+        </Card>
+      </ScrollView>
+    );
+  }
+
+  if (partners.errMess) {
+    return (
+      <ScrollView>
+        <Mission />
+        <Card>
+          <Card.Title>Community Partners</Card.Title>
+          <Card.Divider />
+          <Text>{partners.errMess}</Text>
+        </Card>
+      </ScrollView>
+    );
+  }
 
   return (
     <ScrollView>
@@ -32,10 +57,10 @@ const AboutScreen = () => {
       <Card>
         <Card.Title>Community Partners</Card.Title>
         <Card.Divider />
-        {partners.map((partner) => {
+        {partners.partnersArray.map((partner) => {
           return (
             <ListItem key={partner.id}>
-              <Avatar source={partner.image} rounded />
+              <Avatar source={{ uri: baseUrl + partner.image }} rounded />
               <ListItem.Content>
                 <ListItem.Title>{partner.name}</ListItem.Title>
                 <ListItem.Subtitle>{partner.description}</ListItem.Subtitle>
